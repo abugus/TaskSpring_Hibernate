b70de12bd999f0a4ec60433d9eddab489105ad9e
Abubakar Gusaev <abakar.gusaev@gmail.com>
2024-04-15 14:47:01 +0300

    dсщтconstructor di

--- 2.2.1. spring_hibernate/src/main/java/hiber/service/UserServiceImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/service/UserServiceImp.java
@@ -10,9 +10,11 @@ import java.util.List;
 
 @Service
 public class UserServiceImp implements UserService {
-
+    private final UserDao userDao;
     @Autowired
-    private UserDao userDao;
+    public UserServiceImp(UserDao userDao) {
+        this.userDao = userDao;
+    }
 
     @Transactional
     @Override
@@ -26,7 +28,7 @@ public class UserServiceImp implements UserService {
         return userDao.listUsers();
     }
 
-    @Transactional
+    @Transactional(readOnly = true)
     @Override
     public User getUserByCarModelAndSeries(String model, int series) {
         return userDao.getUserByCarModelAndSeries(model, series);
